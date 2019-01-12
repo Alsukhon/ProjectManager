b11d36b73a07753a6e7d3970af968f798e770e9e
Mohammad Alsukhon <malsukho@emich.edu>
2019-01-11 23:01:34 -0500

    Added Upwork authorization file

---
+++ UpWorkAuthenticate.py
@@ -0,0 +1,9 @@
+import python-upwork
+
+#Instantiating a client without an auth token
+client = upwork.Client(public_key, secret_key)
+
+print "Get to this URL (authorize the app if necessary):"
+print client.auth.get_authorize_url()
+print "After that you should be redirected back to your app URL with " + \
+      "additional ?oauth_verifier= parameter"
